b141c938148899072f0a178239c7ac5dd930960c
Anupom Roy <anupomr@yahoo.com>
2018-06-28 17:57:33 -0400

    Node Class is created

---
+++ DataStructures/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Node
+    {
+        public int Data { get; private set; }
+        public Node Next { get; set; }
+        public Node(int data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
